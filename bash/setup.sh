f4fe88d6fe108af12beb74d1c874590de1c5ac53
Yuri Zats <yuri@zatsweb.com>
2015-05-18 16:14:14 -0700

    add .bash_local with history customizations

--- bash/setup.sh
+++ bash/setup.sh
@@ -9,6 +9,21 @@ then
     cp .bash_aliases ~/
 else
     echo ".bash_aliases already exists"
-fi 
+fi
+
+
+
+if [ ! -e ~/.bash_local ]
+then
+   echo "copying .bash_local"
+   cp .bash_local ~/
+
+   echo -n "Append .bash_local to .bashrc (y/n) ?"
+   read answer
+   if echo "$answer" | grep -iq "^y" ;then
+      cat .bash_local >> ~/.bashrc
+   fi
+else
+   echo ".bash_local already exists"
 
 echo "done"
